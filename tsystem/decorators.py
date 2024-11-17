374e1cf1a5361940b5c92a797f7f017c7001bd43
adragaleff <adragaleff@yandex.ru>
2024-11-17 10:02:47 +0300

    restricted_access.html

--- tsystem/decorators.py
+++ tsystem/decorators.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseForbidden
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 def staff_required(view_func):
     def _wrapper_view(request, *args, **kwargs):
         if not request.user.groups.filter(name='supports').exists():
-            return HttpResponseForbidden('<h1>Доступ ограничен</h1><p>У вас нет прав для доступа к этой странице.</p>')
+            return render(request, 'restricted_access.html')
         return view_func(request, *args, **kwargs)
     return _wrapper_view
