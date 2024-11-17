d2bd0e106fba15162cfdae52136f6a230070b025
adragaleff <adragaleff@yandex.ru>
2024-11-17 09:58:51 +0300

    fix

--- tsystem/decorators.py
+++ tsystem/decorators.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 
 def staff_required(view_func):
     def _wrapper_view(request, *args, **kwargs):
-        if not request.user.groups.filter(name='support').exists():
+        if not request.user.groups.filter(name='supports').exists():
             return HttpResponseForbidden('<h1>Доступ ограничен</h1><p>У вас нет прав для доступа к этой странице.</p>')
         return view_func(request, *args, **kwargs)
     return _wrapper_view
